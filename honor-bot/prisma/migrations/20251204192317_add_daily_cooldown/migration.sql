e1df2773d8d429918ebaa60fed3e5512ab909d15
Sirawitch <sirawitchbu@kkumail.com>
2025-12-05 02:24:53 +0700

    Removed WatchParty, Added Daily & Flip

---
+++ honor-bot/prisma/migrations/20251204192317_add_daily_cooldown/migration.sql
@@ -0,0 +1,12 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN "lastDaily" DATETIME;
+
+-- CreateTable
+CREATE TABLE "Admin" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Admin_username_key" ON "Admin"("username");
